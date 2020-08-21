c67bef4c8f23e346d39c6f4d6e3d20a0997bafc5
Daniel-Hawee <daniel.bologna@community.unipa.it>
2020-08-21 17:48:20 +0200

    Aggiornamenti Layout

--- APP-REACT/air_bdba/src/App.js
+++ APP-REACT/air_bdba/src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+//Quando importi una nuova pagina NON mettere le parentesi graffe!!!
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import NoMatch from './NoMatch';
+import Layout from './components/Layout';
+import NavigationBar from './components/NavigationBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <NavigationBar />
+      <Layout>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Router>
+      </Layout>
+    </React.Fragment>
   );
 }
 
