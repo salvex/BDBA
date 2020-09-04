4408d8f14a8aa163391a0ce1f0b5a1f961289f6b
salvex <salvo.auria@gmail.com>
2020-09-04 14:01:59 +0200

    Sistema di ricerca (vedere se funziona con il front-end)

--- BACK-END/airbdba/app.js
+++ BACK-END/airbdba/app.js
@@ -60,7 +60,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  //res.render('error');
 });
 
 app.listen(PORT, () => console.log("Server startato..porta: " + PORT) );
