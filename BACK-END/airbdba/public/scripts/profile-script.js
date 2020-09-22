f0936a2609a923b590f99275e999004f8dc61874
Daniel-Hawee <daniel.bologna@community.unipa.it>
2020-09-22 12:00:54 +0200

    Trasformato pulsante profile in Home nella pagina user

--- BACK-END/airbdba/public/scripts/profile-script.js
+++ BACK-END/airbdba/public/scripts/profile-script.js
@@ -1,4 +1,11 @@
 $(document).ready(() => {
   /* LETs & CONSTs */
   /* --------------------------------------------------------- */
+
+  /* ON REFRESH */
+  // Modifico la navbar modificando l'opzione profilo,
+  // ridondante in quanto riporta alla stessa pagina
+  // e la trasformo nell'opzione "Home"
+  $("#profile-nav").attr("href", "/").text("Home");
+  /* --------------------------------------------------------- */
 });
